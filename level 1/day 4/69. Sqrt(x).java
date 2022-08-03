cc3197909f2d2f8e424cb06194006c9732c06653
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2022-08-03 23:57:54 +0530

    Create 69. Sqrt(x).java
    Given a non-negative integer x, compute and return the square root of x.
    
    Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
    
    Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
    
     
    
    Example 1:
    
    Input: x = 4
    Output: 2
    Example 2:
    
    Input: x = 8
    Output: 2
    Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.

---
+++ level 1/day 4/69. Sqrt(x).java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l=0;
+        int r=x;
+        if (x<2) return x;
+        while (l<r){
+            int mid=l+(r-l)/2;
+            int temp=x/mid;
+            if(temp==mid){
+                return mid;
+            }
+            else if (temp<mid){
+                r=mid;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return l-1;
+    }
+}
